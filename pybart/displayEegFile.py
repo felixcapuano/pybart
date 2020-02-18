bae35a25c81c4b656b30e1cf830c18c900427b49
felixcapuano <felix.capuano@gmail.com>
2020-02-18 18:01:35 +0100

    ???

--- pybart/displayEegFile.py
+++ pybart/displayEegFile.py
@@ -1,19 +1,23 @@
 import mne
 import numpy as np
 import os
+import time
 
 vhdrPath = 'C:\\Users\\User\\Documents\\InterLabex\\CAPFE_0002.vhdr'
 vmrkPath = 'C:\\Users\\User\\Documents\\InterLabex\\CAPFE_0002.vmrk'
 
 raw = mne.io.read_raw_brainvision(vhdrPath)
 
-start, stop = 20355,20358
+start, stop = 0,400000
 
-period = raw.get_data(start=start,stop=stop,return_times=True)
+period = raw.get_data(start=start,stop=stop)
 
-print(period)
+for i in range(stop-start):
+    p =period[:,i]*10000000000
+    if int(-117937) in p.astype(int) and int(1672) in p.astype(int):
+        print(p.astype(int))
 
-
+    # time.sleep(0.10)
 
 # events_orig = mne.events_from_annotations(raw, event_id='auto')[0] 
 # events = np.copy(events_orig)    
